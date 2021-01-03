282651cf4b69aa282dbbac7193bb62bfc1a99f40
Vicky Kumar <imvickykumar999@gmail.com>
2021-01-03 17:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ socketio = SocketIO(app)
 def sessions():
     return render_template('session.html')
 
-
+@app.route('/', methods=['GET', 'POST'])
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['POST']):
+def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
